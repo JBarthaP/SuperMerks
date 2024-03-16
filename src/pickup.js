8683776f9882bc32069d206a082a097fed33f22c
jbarthad <jbarthap@gmail.com>
2024-03-16 23:33:30 +0100

    Objetos para pasar nota y logica paso de nota
    Ademas incluido las animaciones de cada objeto
    Falta randomizar donde salen los pick ups

--- src/pickup.js
+++ src/pickup.js
@@ -3,12 +3,21 @@ import Phaser from 'phaser'
 
 export default class PickUp extends Phaser.GameObjects.Sprite {
 
-    constructor(scene, x, y, sprite, mark) {
+    constructor(scene, x, y, sprite,frames, mark) {
         super(scene, x, y, sprite);
         this.scene = scene
         this.scene.add.existing(this);
         this.scene.physics.add.existing(this, true);
         this.mark = mark
+
+        this.scene.anims.create({
+            key: sprite,
+            frames: this.scene.anims.generateFrameNumbers(sprite, {start: 0, end: frames-1}),
+            frameRate: frames-1,
+            repeat: -1
+        });
+
+        this.play(sprite, true)
     }
 
     /**
--- src/scoreManager.js
+++ src/scoreManager.js
@@ -10,7 +10,7 @@ const PUNTUACION = {
     B: 70,
     A: 90,
     S: 100
-  };
+};
 
 export default class ScoreManager {
 
@@ -18,76 +18,83 @@ export default class ScoreManager {
         this.scene = scene
         this.player = player
         this.score = 30
-        this.currentMark = PUNTUACION.F 
+        this.currentMark = PUNTUACION.F
         this.numberHitsInF = 0
-        this.assignObjectsToCap() 
-       
+        this.assignObjectsToCap()
+        this.actualPickUp;
+
         //Provisional
         this.label = this.scene.add.text(10, 10, "").setDepth(2);
-        this.labelNota = this.scene.add.text(10,60, "").setDepth(2);
+        this.labelNota = this.scene.add.text(10, 60, "").setDepth(2);
         this.updateScore()
     }
 
-    assignObjectsToCap()
-    {
+    assignObjectsToCap() {
         this.objectsDict = {
-            [PUNTUACION.D]: {texture: 'spritesheet_d', frames: 7},
-            [PUNTUACION.C]: {texture: 'spritesheet_c', frames: 8},
-            [PUNTUACION.B]: {texture: 'spritesheet_b', frames: 12},
-            [PUNTUACION.A]: {texture: 'spritesheet_a', frames: 8},
-            [PUNTUACION.S]: {texture: 'spritesheet_s', frames: 10},
+            [PUNTUACION.D]: { texture: 'spritesheet_d', frames: 7 },
+            [PUNTUACION.C]: { texture: 'spritesheet_c', frames: 8 },
+            [PUNTUACION.B]: { texture: 'spritesheet_b', frames: 12 },
+            [PUNTUACION.A]: { texture: 'spritesheet_a', frames: 8 },
+            [PUNTUACION.S]: { texture: 'spritesheet_s', frames: 10 },
         }
     }
 
-    addPoints(points)
-    {
+    addPoints(points) {
         const newScore = this.score + points;
-        if(this.currentMark === PUNTUACION.F && newScore >= PUNTUACION.D)
-        {
+        if (this.currentMark === PUNTUACION.F && newScore >= PUNTUACION.D) {
             this.score = PUNTUACION.D
             this.passUpCap(PUNTUACION.D)
 
-        } else if(this.currentMark === PUNTUACION.D && newScore >= PUNTUACION.C)
-        {
+        } else if (this.currentMark === PUNTUACION.D && newScore >= PUNTUACION.C) {
             this.score = PUNTUACION.C
             this.passUpCap(PUNTUACION.C)
 
-        } else if(this.currentMark === PUNTUACION.C && newScore >= PUNTUACION.B)
-        {
+        } else if (this.currentMark === PUNTUACION.C && newScore >= PUNTUACION.B) {
             this.score = PUNTUACION.B
             this.passUpCap(PUNTUACION.B)
 
-        } else if(this.currentMark === PUNTUACION.B && newScore >= PUNTUACION.A)
-        {
+        } else if (this.currentMark === PUNTUACION.B && newScore >= PUNTUACION.A) {
             this.score = PUNTUACION.A
             this.passUpCap(PUNTUACION.A)
 
-        } else if(this.currentMark === PUNTUACION.A && newScore >= PUNTUACION.S)
-        {
+        } else if (this.currentMark === PUNTUACION.A && newScore >= PUNTUACION.S) {
             this.score = PUNTUACION.S
             this.passUpCap(PUNTUACION.S)
 
         } else {
             this.score += points
-        } 
+        }
         this.updateScore()
 
     }
 
-    reduceScore(points)
-    {
+    reduceScore(points) {
+        const newScore = this.score / points;
+        if (this.currentMark === PUNTUACION.D && newScore <= PUNTUACION.D) {
+            this.currentMark = PUNTUACION.F
+        } else if (this.currentMark === PUNTUACION.C && newScore <= PUNTUACION.C) {
+            this.currentMark = PUNTUACION.D
+        } else if (this.currentMark === PUNTUACION.B && newScore <= PUNTUACION.B) {
+            this.currentMark = PUNTUACION.C
+        } else if (this.currentMark === PUNTUACION.A && newScore <= PUNTUACION.A) {
+            this.currentMark = PUNTUACION.B
+        } else if (this.currentMark === PUNTUACION.S && newScore <= PUNTUACION.S) {
+            this.currentMark = PUNTUACION.A
+        }
         this.score -= points;
+        this.passDownCap()
         this.updateScore()
     }
 
-    passUpCap(newMark)
-    {
-        const pickUp = new PickUp(this.scene,100,100,this.objectsDict[newMark], newMark)
+    passUpCap(newMark) {
+        this.actualPickUp = new PickUp(this.scene, 100, 100, this.objectsDict[newMark].texture, this.objectsDict[newMark].frames, newMark)
     }
 
-    passDownCap()
-    {
-
+    passDownCap() {
+        if(this.actualPickUp)
+        {
+            this.actualPickUp.destroy()
+        }
     }
 
     updateScore() {
@@ -97,9 +104,4 @@ export default class ScoreManager {
     }
 
     generateCapObject()
-    {
-
-    }
-
-
 }
