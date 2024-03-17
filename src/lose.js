2cd5ce4dce24986772d89a86db1a5c6be1e82228
MRM4R10 <magodesauni@gmail.com>
2024-03-17 18:38:07 +0100

    Mondongo

--- src/lose.js
+++ src/lose.js
@@ -17,20 +17,27 @@ export default class Lose extends Phaser.Scene {
    * Creación de la escena. Tan solo contiene el texto que indica que el juego se ha acabado
    */
   create() {
-    this.add.text(500, 250, 'Se acabó!\nPulsa cualquier tecla para volver a jugar')
-        .setOrigin(0.5, 0.5)  // Colocamos el pivote en el centro de cuadro de texto 
-        .setAlign('center');  // Centramos el texto dentro del cuadro de texto
+    this.add.text(85, 100, 'Nos vemos en septiembre crack').setOrigin(0).setAlign('center').setFontSize(55).setColor('#f55363');
+    this.add.text(95, 200, 'No has conseguido aprobar la práctica ').setOrigin(0).setAlign('center').setFontSize(30);
+    this.add.text(95, 230, 'del laboratorio.').setOrigin(0).setAlign('center').setFontSize(30);
+    this.nota_f = this.add.image(this.game.renderer.width - 200, this.game.renderer.height - 200, "nota_f").setScale(4);
 
-    // Añadimos el listener para cuando se haya pulsado una tecla. Es probable que no
-    // lleguemos a ver el mensaje porque veníamos con una tecla pulsada del juego (al 
-    // ir moviendo al jugador). Se puede mejorar añadiendo un temporizador que 
-    // añada este listener pasado un segundo
-    this.time.delayedCall(2000, () => {
-      this.input.keyboard.on('keydown', function (event) { 
-        this.scene.start('level');
-      }, this);
 
-    })
+    this.back_to_menu = this.add.text(95, 450, '<Pagar segunda matrícula>').setOrigin(0).setAlign('center').setFontSize(30).setColor('#42ebcc');
+    this.back_to_menu.setInteractive();
+    this.back_to_menu.on("pointerup", () => {
+      this.sound.stopAll();
+      this.scene.start("mainMenuScene");
+    });
+
+    this.back_to_menu.on('pointerover', function (pointer) {
+      this.setColor('#f55363');
+    });
+
+    this.back_to_menu.on('pointerout', function (pointer) {
+      this.setColor('#42ebcc');
+    });
+
   }
 
 }
